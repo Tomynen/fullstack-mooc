ad920f521f36129948241b33335118975b2be608
Tomynen <tomipjturunen@gmail.com>
2025-04-10 20:08:00 +0300

    2e jatkuu

--- osa2/maiden-tiedot/src/components/FilteredCountries.jsx
+++ osa2/maiden-tiedot/src/components/FilteredCountries.jsx
@@ -1,20 +1,17 @@
 const FilteredCountries = ({countries}) => {
-
-    // if(countries.length == 1){
-    //     return null
-    // }
-    console.log(countries[0])
     if(countries.length > 10){
         return(
-            <p>Too many countries with the filter add more search text</p>
+            <p>Too many countries {countries.length} with the filter add more search text</p>
         )
-    }else{
+    }else if(!countries || countries.length == 0)
+    {
+        return null
+    }
+    else{
         return(
             <>
                 <ul>
-                    {countries.map(c => {
-                        <li>{c.name.common}</li>
-                    })}
+                    {countries.map(c => <li key={c.name.common}>{c.name.common}</li>)}
                 </ul>
             </>
         )
